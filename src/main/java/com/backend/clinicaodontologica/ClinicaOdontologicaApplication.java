1c8d644a0265cf6a7fb0190c64baf8d7cebc30a6
TomasTozzi <tozzitomas@gmail.com>
2023-11-09 21:27:01 -0300

    ya arranca

---
+++ src/main/java/com/backend/clinicaodontologica/ClinicaOdontologicaApplication.java
@@ -0,0 +1,52 @@
+package com.backend.clinicaodontologica;
+
+import org.modelmapper.ModelMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+
+@SpringBootApplication
+public class ClinicaOdontologicaApplication {
+
+	private static Logger logger = LoggerFactory.getLogger(ClinicaOdontologicaApplication.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicaOdontologicaApplication.class, args);
+		crearTabla();
+		logger.info("ClinicaOdontologica is now running...");
+	}
+
+
+
+	@Bean
+	public ModelMapper modelMapper(){
+		return new ModelMapper();
+	}
+
+
+
+	private static void crearTabla(){
+		Connection connection = null;
+		try {
+			Class.forName("org.h2.Driver");
+			connection = DriverManager.getConnection("jdbc:h2:~/clinicaC1;INIT=RUNSCRIPT FROM 'create.sql'", "sa", "sa");
+
+		} catch (Exception e){
+			e.printStackTrace();
+		} finally {
+			try {
+				connection.close();
+			} catch (Exception ex){
+				ex.printStackTrace();
+			}
+		}
+	}
+
+
+}
---
+++ src/main/java/com/backend/clinicaodontologica/service/IPacienteService.java
@@ -0,0 +1,15 @@
+package com.backend.clinicaodontologica.service;
+
+import com.backend.clinicaodontologica.dto.entrada.paciente.PacienteEntradaDto;
+import com.backend.clinicaodontologica.dto.salida.paciente.PacienteSalidaDto;
+import com.backend.clinicaodontologica.model.Paciente;
+
+import java.util.List;
+
+public interface IPacienteService {
+    PacienteSalidaDto registrarPaciente(PacienteEntradaDto paciente);
+
+    List<PacienteSalidaDto> listarPacientes();
+    PacienteSalidaDto buscarPacientePorId(int id);
+    Paciente actualizarPaciente(Paciente paciente);
+}
